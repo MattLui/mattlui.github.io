855594ef8ffef7e67f2ae68d2a7c995229ea9749
Matt Lui <Mattclui@gmail.com>
2025-01-16 18:37:54 -0800

    mixpanel tracking for page duration and link clicks

--- assets/js/mixpanel.js
+++ assets/js/mixpanel.js
@@ -5,3 +5,25 @@ mixpanel.init("7a72925a61bd3ba2278e2da6091e3d3c", {
 });
 
 mixpanel.track("Page Loaded");
+
+// Page Duration
+mixpanel.time_event("Page Duration");
+
+window.addEventListener("beforeunload", () => {
+  mixpanel.track("Page Duration", {
+    page: document.title,
+  });
+});
+
+// Link Click
+document.addEventListener("click", (event) => {
+  const target = event.target;
+
+  if (target.tagName === "A") {
+    mixpanel.track("Link Clicked", {
+      href: target.href,
+      link_text: target.textContent.trim(),
+      link_target: target.target || "_self",
+    });
+  }
+});
